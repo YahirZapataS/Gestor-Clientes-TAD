8e29750bbadd343436d55139f1a300f43c18fa74
YahirZapataS <yahirzapatae@gmail.com>
2025-07-20 15:10:28 -0600

    Subida de proyecto

---
+++ gestor-clientes-tad/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-2x1 text-blue-500 p-4">
+    ¡Tailwind Funcionando!
+  </div>
+</template>
---
+++ gestor-clientes-tad/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-4">
+        <h1 class="text-x1 font-bold">Inicio</h1>
+    </div>
+</template>
